256e82e0fcfd4d659d911d990fbef9dbdff9e0d1
Marco Bissaro <marco.bissaro@dorbit.space>
2025-06-10 15:02:09 +0200

    added day 2 input & solutions

--- src/solver/solver2015.py
+++ src/solver/solver2015.py
@@ -1,6 +1,7 @@
 class Solver():
     def __init__(self, *args, **kwargs):
         self.first_sol, self.second_sol = None, None
+        self.input = ""
         
     def d1_p1(self)->None:
         self.first_sol = 0
@@ -26,4 +27,21 @@ class Solver():
             if current_pos < 0: 
                 self.second_sol = step + 1
                 return
-            
+    def d2_p1(self) -> None:
+        self.first_sol = 0
+        for element in self.input.split("\n"):
+            element = [int(number) for number in element.split("x")]
+            sides_area = [element[0] * element[1], element[1] * element[2], element[2] * element[0]]
+            self.first_sol += 2 * sum(sides_area) + min(sides_area)
+
+    def d2_p2(self) -> None:
+        self.second_sol = 0
+        for sides in self.input.split("\n"):
+            sides = [int(number) for number in sides.split("x")]
+            ribbon_length = 1
+            for side in sides:
+                ribbon_length = ribbon_length * side
+            smallest_perimeter = sides.remove(max(sides))
+            ribbon_length += 2*sum(sides)
+            self.second_sol += ribbon_length
+        
